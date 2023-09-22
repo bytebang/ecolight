14c0106cd143f596e3cb454f834f8b3db60bdd5d
Günther Hutter <guenther.hutter@htl-leoben.at>
2023-09-22 09:11:51 +0200

    Added aggregator spring application

---
+++ aggregator/src/test/java/at/htlle/aggregator/AggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
