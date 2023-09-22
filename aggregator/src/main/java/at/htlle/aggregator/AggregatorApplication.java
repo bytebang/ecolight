9d4d70c732570245a2355920e99c0dfa55581059
Günther Hutter <guenther.hutter@bytebang.at>
2023-09-22 23:52:18 +0200

    Removed all warnings

--- aggregator/src/main/java/at/htlle/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/at/htlle/aggregator/AggregatorApplication.java
@@ -2,13 +2,10 @@ package at.htlle.aggregator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class AggregatorApplication {
