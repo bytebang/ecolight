16bb5b96a068adf9c3eb707491a218fe29e53af5
Günther Hutter <guenther.hutter@bytebang.at>
2023-09-22 22:53:07 +0200

    Initial implementation of a CRUD Webservice with spring

--- aggregator/src/main/java/at/htlle/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/at/htlle/aggregator/AggregatorApplication.java
@@ -9,5 +9,4 @@ public class AggregatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AggregatorApplication.class, args);
 	}
-
 }
---
+++ aggregator/src/main/java/at/htlle/aggregator/Person.java
@@ -0,0 +1,33 @@
+package at.htlle.aggregator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
