ac693f7bfbd75c7958bcf9d6d0b2ee6643b92021
191wita05 <191wita05@o365.htl-leoben.at>
2023-11-24 11:01:49 +0100

    created RESTController.java

--- dashboard/src/main/java/at/htlle/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/at/htlle/dashboard/DashboardApplication.java
@@ -11,29 +11,3 @@ public class DashboardApplication {
 	}
 
 }
-/*
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class MyController {
-
-    @GetMapping("/home")
-    public String home(Model model) {
-        // Add model attributes if needed for rendering HTML
-        return "home"; // Thymeleaf template name (e.g., home.html)
-    }
-
-    @PostMapping("/receive-data")
-    @ResponseBody
-    public String receiveData(@RequestBody String data) {
-        // Process the received data
-        System.out.println("Received data: " + data);
-        return "Data received successfully!";
-    }
-}
-*/
